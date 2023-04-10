10e05af71e1818c40b0e84ebfec729b2ec1e6c5f
Pranit Darekar <pranitdarekar1997@gmail.com>
2023-04-10 17:40:22 -0400

    changes

--- app/src/main/java/com/example/socialmedia/OtherUser.java
+++ app/src/main/java/com/example/socialmedia/OtherUser.java
@@ -2,13 +2,72 @@ package com.example.socialmedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.Window;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class OtherUser extends AppCompatActivity {
+
+    Toolbar toolbar;
+    String otherUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_other_user);
+
+        otherUser = getIntent().getStringExtra("searched_username");
+
+        toolbar = findViewById(R.id.toolbar_other_user);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(otherUser);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        SubMenu subMenu = menu.findItem(R.id.main_menu).getSubMenu();
+        for (int i = 0; i < subMenu.size(); i++) {
+            MenuItem subMenuItem = subMenu.getItem(i);
+            subMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    // Handle submenu item tap
+                    switch (item.getItemId()) {
+                        case R.id.add_posts:
+                            Intent addPostIntent = new Intent(getApplicationContext(), AddPost.class);
+                            startActivity(addPostIntent);
+                            finish();
+                            break;
+                        case R.id.logout:
+                            FirebaseAuth.getInstance().signOut();
+                            Intent logoutIntent = new Intent(getApplicationContext(), Login.class);
+                            startActivity(logoutIntent);
+                            finish();
+                            break;
+                        case R.id.edit_profile:
+                            Intent editProfileIntent = new Intent(getApplicationContext(), EditProfile.class);
+                            startActivity(editProfileIntent);
+                            finish();
+                            break;
+                        case R.id.search_user:
+                            Intent searchUserIntent = new Intent(getApplicationContext(), SearchUser.class);
+                            startActivity(searchUserIntent);
+                            finish();
+                            break;
+                    }
+                    return true;
+                }
+            });
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/socialmedia/SearchUser.java
+++ app/src/main/java/com/example/socialmedia/SearchUser.java
@@ -2,6 +2,9 @@ package com.example.socialmedia;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -79,7 +82,7 @@ public class SearchUser extends AppCompatActivity {
                                             && result.getSignInMethods().contains(EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD)) {
                                         // User exists
                                         Intent intent = new Intent(SearchUser.this, OtherUser.class);
-                                        intent.putExtra("seachedUsername", username);
+                                        intent.putExtra("searched_username", username);
                                         startActivity(intent);
                                         finish();
                                     } else {
@@ -94,5 +97,45 @@ public class SearchUser extends AppCompatActivity {
                         });
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        SubMenu subMenu = menu.findItem(R.id.main_menu).getSubMenu();
+        for (int i = 0; i < subMenu.size(); i++) {
+            MenuItem subMenuItem = subMenu.getItem(i);
+            subMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    // Handle submenu item tap
+                    switch (item.getItemId()) {
+                        case R.id.add_posts:
+                            Intent addPostIntent = new Intent(getApplicationContext(), AddPost.class);
+                            startActivity(addPostIntent);
+                            finish();
+                            break;
+                        case R.id.logout:
+                            FirebaseAuth.getInstance().signOut();
+                            Intent logoutIntent = new Intent(getApplicationContext(), Login.class);
+                            startActivity(logoutIntent);
+                            finish();
+                            break;
+                        case R.id.edit_profile:
+                            Intent editProfileIntent = new Intent(getApplicationContext(), EditProfile.class);
+                            startActivity(editProfileIntent);
+                            finish();
+                            break;
+                        case R.id.search_user:
+                            Intent searchUserIntent = new Intent(getApplicationContext(), SearchUser.class);
+                            startActivity(searchUserIntent);
+                            finish();
+                            break;
+                    }
+                    return true;
+                }
+            });
+        }
+        return true;
     }
 }
