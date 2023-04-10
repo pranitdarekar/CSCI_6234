bfe64fee0bad76f7451e950f8c539f779db1d854
Pranit Darekar <pranitdarekar1997@gmail.com>
2023-04-10 12:57:44 -0400

    changes

--- app/src/main/java/com/example/socialmedia/Register.java
+++ app/src/main/java/com/example/socialmedia/Register.java
@@ -89,6 +89,7 @@ public class Register extends AppCompatActivity {
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                finish();
                             }
                         }
                     });
