908b7cc020024ae970d17d9199e7fd3c743d93ab
Pranit Darekar <pranitdarekar1997@gmail.com>
2023-04-11 04:40:39 -0400

    changes

--- app/src/main/java/com/example/socialmedia/MainActivity.java
+++ app/src/main/java/com/example/socialmedia/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.socialmedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser user;
 
     Toolbar toolbar;
+    ImageAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(user.getEmail());
+
+        recyclerView = findViewById(R.id.images_grid_main);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        adapter = new ImageAdapter();
+        recyclerView.setAdapter(adapter);
+
+        displayPosts();
     }
 
     @Override
@@ -84,5 +112,104 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         return true;
+    }
+
+    private void displayPosts() {
+        FirebaseDatabase.getInstance().getReference().child("uploads").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Clear the list to avoid duplicates
+                adapter.clear();
+
+                // Loop through all the images in the database
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    Map<String, Object> userMap = (Map<String, Object>) snapshot.getValue();
+
+                    // Create a JSONObject from the userMap
+                    JSONObject postsObj = new JSONObject(userMap);
+                    try {
+                        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                        if (postsObj.has("imageUrl") && !postsObj.getString("uid").equals(userId)) {
+                            Image image = new Image(postsObj.getString("imageUrl"));
+                            adapter.addImage(image);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle errors here
+            }
+        });
+    }
+
+    private class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+        private List<Image> mImages;
+
+        public ImageAdapter() {
+            mImages = new ArrayList<>();
+        }
+
+        public void clear() {
+            mImages.clear();
+            notifyDataSetChanged();
+        }
+
+        public void addImage(Image image) {
+            mImages.add(image);
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public ImageAdapter.ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.cardview_image, parent, false);
+            return new ImageAdapter.ImageViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ImageAdapter.ImageViewHolder holder, int position) {
+            Image image = mImages.get(position);
+
+            // Load the image using Glide or Picasso
+            Glide.with(holder.itemView.getContext())
+                    .load(image.getUrl())
+                    .into(holder.imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mImages.size();
+        }
+
+        private class ImageViewHolder extends RecyclerView.ViewHolder {
+            private ImageView imageView;
+
+            public ImageViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.image_view);
+            }
+        }
+    }
+
+    private class Image {
+        private String url;
+
+        public Image(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
     }
 }
