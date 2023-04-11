78f1d0385ba2e530d25963b8bbfd451352e2fdb7
Pranit Darekar <pranitdarekar1997@gmail.com>
2023-04-11 02:55:46 -0400

    changes

--- app/src/main/java/com/example/socialmedia/MyPage.java
+++ app/src/main/java/com/example/socialmedia/MyPage.java
@@ -7,14 +7,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -28,6 +33,14 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 
 public class MyPage extends AppCompatActivity {
 
@@ -35,6 +48,8 @@ public class MyPage extends AppCompatActivity {
     TextView bioTextView;
     String userId;
     ImageView profilePictureImage;
+    RecyclerView recyclerView;
+    ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +61,11 @@ public class MyPage extends AppCompatActivity {
         bioTextView = findViewById(R.id.bio_text_view);
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         profilePictureImage = findViewById(R.id.profile_image_view);
+
+        recyclerView = findViewById(R.id.images_grid);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        adapter = new ImageAdapter();
+        recyclerView.setAdapter(adapter);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
@@ -102,6 +122,7 @@ public class MyPage extends AppCompatActivity {
     private void updateProfileComponents() {
         updateBioText();
         updateProfilePicture();
+        displayPosts();
     }
 
     private void updateBioText() {
@@ -140,5 +161,103 @@ public class MyPage extends AppCompatActivity {
                 Log.e("FirebaseError", "Error getting download URL: " + e.getMessage());
             }
         });
+    }
+
+    private void displayPosts() {
+        FirebaseDatabase.getInstance().getReference().child("uploads").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Clear the list to avoid duplicates
+                adapter.clear();
+
+                // Loop through all the images in the database
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    Map<String, Object> userMap = (Map<String, Object>) snapshot.getValue();
+
+                    // Create a JSONObject from the userMap
+                    JSONObject postsObj = new JSONObject(userMap);
+                    try {
+                        if (postsObj.has("imageUrl") && Objects.equals(userId, postsObj.getString("uid"))) {
+                            Image image = new Image(postsObj.getString("imageUrl"));
+                            adapter.addImage(image);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle errors here
+            }
+        });
+    }
+
+    private class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+        private List<Image> mImages;
+
+        public ImageAdapter() {
+            mImages = new ArrayList<>();
+        }
+
+        public void clear() {
+            mImages.clear();
+            notifyDataSetChanged();
+        }
+
+        public void addImage(Image image) {
+            mImages.add(image);
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public ImageAdapter.ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.cardview_image, parent, false);
+            return new ImageAdapter.ImageViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ImageAdapter.ImageViewHolder holder, int position) {
+            Image image = mImages.get(position);
+
+            // Load the image using Glide or Picasso
+            Glide.with(holder.itemView.getContext())
+                    .load(image.getUrl())
+                    .into(holder.imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mImages.size();
+        }
+
+        private class ImageViewHolder extends RecyclerView.ViewHolder {
+            private ImageView imageView;
+
+            public ImageViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.image_view);
+            }
+        }
+    }
+
+    private class Image {
+        private String url;
+
+        public Image(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
     }
 }
